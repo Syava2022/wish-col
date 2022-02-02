3ae9273e00ba84faf8d34ec71557d478077c4e72
Oleg <oleshka3000@yahoo.com>
2022-02-02 12:56:20 +0200

    create component App.js

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <>
+            <h1>WEB Application - Wish collector</h1>
+            <p>Wishes collection</p>
+        </>
+    )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import App from 'components/App'
 
-const el = <h1>Wish Collector</h1>
-
-ReactDom.render(el, document.getElementById('root'))
+ReactDom.render(<App />, document.getElementById('root'))
